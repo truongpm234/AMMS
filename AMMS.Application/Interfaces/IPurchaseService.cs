e2eff2505f0cc453b717fef86be822143be8fdb7
truongpm234 <phamminhtruong2032004@gmail.com>
2025-12-26 18:06:56 +0700

    [FIX][DealService]: send mail and payos

--- AMMS.Application/Interfaces/IPurchaseService.cs
+++ AMMS.Application/Interfaces/IPurchaseService.cs
@@ -1,36 +1,15 @@
-﻿using AMMS.Shared.DTOs.Common;
+﻿using AMMS.Infrastructure.Entities;
+using AMMS.Shared.DTOs.Common;
 using AMMS.Shared.DTOs.Purchases;
 
 namespace AMMS.Application.Interfaces
 {
     public interface IPurchaseService
     {
-        Task<CreatePurchaseRequestResponse> CreatePurchaseRequestAsync(
-            CreatePurchaseRequestDto dto,
-            int? createdBy,
-            CancellationToken ct = default);
-
-<<<<<<< HEAD
-        Task<PagedResultLite<PurchaseOrderListItemDto>> GetPurchaseOrdersAsync(
-            int page, int pageSize, CancellationToken ct = default);
-
-=======
-        Task<PagedResultLite<PurchaseOrderWithItemsDto>> GetPurchaseOrdersAsync(
-                    int page, int pageSize, CancellationToken ct = default);
->>>>>>> main
-        Task<PurchaseOrderListItemDto> CreatePurchaseOrderAsync(
-            CreatePurchaseRequestDto dto,
-            CancellationToken ct = default);
-
-        // ✅ CHANGED: receive theo purchaseId
+        Task<CreatePurchaseRequestResponse> CreatePurchaseRequestAsync(CreatePurchaseRequestDto dto, int? createdBy, CancellationToken ct = default);
+        Task<PagedResultLite<PurchaseOrderListItemDto>> GetPurchaseOrdersAsync(int page, int pageSize, CancellationToken ct = default);
+        Task<PagedResultLite<PurchaseOrderListItemDto>> GetPendingPurchasesAsync(int page, int pageSize, CancellationToken ct = default);
+        Task<PurchaseOrderListItemDto> CreatePurchaseOrderAsync(CreatePurchaseRequestDto dto, CancellationToken ct = default);
         Task<object> ReceiveAllPendingPurchasesAsync(int purchaseId, CancellationToken ct = default);
-
-<<<<<<< HEAD
-        // ✅ CHANGED: pending paging
-        Task<PagedResultLite<PurchaseOrderListItemDto>> GetPendingPurchasesAsync(
-=======
-        Task<PagedResultLite<PurchaseOrderWithItemsDto>> GetPendingPurchasesAsync(
->>>>>>> main
-            int page, int pageSize, CancellationToken ct = default);
     }
 }
